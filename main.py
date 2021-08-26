535ef059a920792143fd0429a316c97ba3b141ef
raju <dahalraju55@gmail.com>
2021-08-26 18:46:06 +0545

    assigned single function call to handle all incoming requests

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request
 
 main = Flask(__name__)
 
 
 @main.route('/')
 def home():
+    # req2 = request.script_root
+    # print(req2, "HERE")
     return render_template('index.html')
 
 
-@main.route('/services')
-def services():
-    return render_template('services.html')
+@main.route('/<url>')
+def redirect(url):
+    path = request.path
+    url = path[1:len(path)]
+    file = f'{url}.html'
+    return render_template(file)
 
 
-@main.route('/corporate')
-def corporate():
-    return render_template('b2b.html')
+if __name__ == '__main__':
+    main.run()
 
-
-@main.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-
-@main.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@main.route('/macbook')
-def macbook():
-    return render_template('macbook.html')
-# @main.route('/services')
-
-# if __name__ == '__main__':
-#     app.run()
+# # @main.route('/services')
+# # def services():
+# #     return render_template('services.html')
+#
+#
+# @main.route('/corporate')
+# def corporate():
+#     return render_template('b2b.html')
+#
+#
+# @main.route('/contact')
+# def contact():
+#     return render_template('contact.html')
+#
+#
+# @main.route('/about')
+# def about():
+#     return render_template('about.html')
+#
+#
+# @main.route('/macbook')
+# def macbook():
+#     return render_template('macbook.html')
+# # @main.route('/services')
+#
+# # if __name__ == '__main__':
+# #     app.run()
