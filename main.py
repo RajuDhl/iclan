fb766788c410e87d31e3a79aa90da97f68fd443c
raju <dahalraju55@gmail.com>
2021-09-15 18:48:21 +0530

    Some Changes on Nav and GUI

--- main.py
+++ main.py
@@ -22,30 +22,4 @@ def redirect(url):
 if __name__ == '__main__':
     main.run()
 
-# # @main.route('/services')
-# # def services():
-# #     return render_template('services.html')
-#
-#
-# @main.route('/corporate')
-# def corporate():
-#     return render_template('b2b.html')
-#
-#
-# @main.route('/contact')
-# def contact():
-#     return render_template('contact.html')
-#
-#
-# @main.route('/about')
-# def about():
-#     return render_template('about.html')
-#
-#
-# @main.route('/macbook')
-# def macbook():
-#     return render_template('macbook.html')
-# # @main.route('/services')
-#
-# # if __name__ == '__main__':
-# #     app.run()
+
